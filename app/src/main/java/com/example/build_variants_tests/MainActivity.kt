67b6921893f41a77ff4c4ac30a938b737a80ffea
MiltonDevSis <milton.dev.sis@gmail.com>
2021-05-20 21:44:17 -0300

    Codigo refatorado 100% e funcional

--- app/src/main/java/com/example/build_variants_tests/MainActivity.kt
+++ app/src/main/java/com/example/build_variants_tests/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.build_variants_tests
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_free)
+        setContentView(R.layout.activity_main)
+
+        val botaoUm = findViewById<Button>(R.id.btnOne)
+        val botaoDois = findViewById<Button>(R.id.btnTwo)
+        val botaoTres = findViewById<Button>(R.id.btnTree)
+
+        botaoUm.setOnClickListener {
+            Toast.makeText(applicationContext, R.string.clickOne, Toast.LENGTH_LONG).show()
+        }
+
+        botaoDois.setOnClickListener {
+            Toast.makeText(applicationContext, R.string.clickTwo, Toast.LENGTH_LONG).show()
+        }
+
+        botaoTres.setOnClickListener {
+            Toast.makeText(applicationContext, R.string.clickTree, Toast.LENGTH_LONG).show()
+        }
 
 
     }
